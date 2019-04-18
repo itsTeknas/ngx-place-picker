1654255e50e1a7f55395294c0e3de8e30e106b13
Sanket Berde <sanket.berde@gmail.com>
2019-04-18 22:58:21 +0530

    added rudimentary search

--- projects/place-picker/src/lib/place-picker.component.ts
+++ projects/place-picker/src/lib/place-picker.component.ts
@@ -1,5 +1,7 @@
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { tap, debounce, debounceTime, map, filter } from 'rxjs/operators';
 import { Location } from './location';
-import { Component, OnInit, Input, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 declare var google: any;
 
@@ -35,21 +37,65 @@ export class PlacePickerComponent implements OnInit, AfterViewInit {
   private map: ElementRef;
 
   private googleMap: any;
+  private placesSearchService: any;
+  private search$: BehaviorSubject<string> = new BehaviorSubject<string>(null);
+  search = '';
+
+  public searchResults = [];
 
   constructor() { }
 
   ngOnInit() {
     this.location = this.defaultLocation;
+    this.search$.pipe(
+      filter((query) => !!query),
+      debounceTime(200),
+      tap((search) => {
+        console.log('Searching: ', search);
+      }),
+      map((searchQuery) => {
+        const placesRequest = {
+          query: searchQuery,
+          fields: ['name', 'geometry'],
+        };
+        this.placesSearchService.findPlaceFromQuery(placesRequest, (results, status) => {
+          this.searchResults = results;
+          console.log(results);
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            this.googleMap.setCenter(results[0].geometry.location);
+          }
+        });
+      })
+
+    ).subscribe((ok) => {
+
+    });
   }
 
   ngAfterViewInit() {
-    if (google) {
+    if (window.hasOwnProperty('google')) {
       this.googleMap = new google.maps.Map(this.map.nativeElement, {
         center: { lat: this.defaultLocation.lat, lng: this.defaultLocation.lng },
         zoom: 8
       });
+
+      if (google.maps.hasOwnProperty('places')) {
+        this.placesSearchService = new google.maps.places.PlacesService(this.googleMap);
+      } else {
+        console.error('Places API not enabled or present in the script import');
+      }
+
     } else {
-      console.error("Google Maps Client not loaded properly, include the script tag in your index.html")
+      console.error('Google Maps Client not loaded properly');
+      console.error('Include the script tag for google maps with the API Key in your index.html');
     }
+  }
+
+  clearSearch() {
+    this.searchResults = [];
+  }
+
+  searchChange($event) {
+    this.search$.next(this.search);
   }
 }
--- projects/place-picker/src/lib/place-picker.module.ts
+++ projects/place-picker/src/lib/place-picker.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PlacePickerComponent } from './place-picker.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     PlacePickerComponent
   ],
   imports: [
+    CommonModule,
+    FormsModule
   ],
   exports: [
     PlacePickerComponent
